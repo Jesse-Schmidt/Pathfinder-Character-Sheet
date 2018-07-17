7bcaed0254139d16c5562f53d39d9750cf15c482
Jesse Schmidt <schmidt.j.jesse@gmail.com>
2018-07-17 11:24:33 -0600

    add basic functionality

---
+++ Class.py
@@ -0,0 +1,60 @@
+from Skills import *
+from Race import *
+
+def determineClass(classType):
+    classes = {"fighter": Fighter()}
+    return classes[classType]
+
+class Class:
+    def __init__(self):
+        self.BAB = 0
+        self.fortitude = 0
+        self.reflex = 0
+        self.will = 0
+        self.skills = [Acrobatics, Appraise, Bluff, Climb, Craft, Diplomacy, Disable_Device,
+                       Disguise, Escape_Artist, Fly, Handle_Animal, Heal, Intimidate, Knowledge,
+                       Linguistics, Perception, Preform, Profession, Ride, Sense_Motive,
+                       Sleight_Of_Hand, Spellcraft, Stealth, Survival, Swim, Use_Magic_Device]
+        self.addedSpeed = 0
+        self.proficientWeapons = []
+        self.hitDice = 0
+
+    def getBAB(self):
+        return self.BAB
+
+    def getFortitude(self):
+        return self.fortitude
+
+    def getWill(self):
+        return self.will
+
+    def getSkills(self):
+        return self.skills
+
+    def getAddedSpeed(self):
+        return self.addedSpeed
+
+    def getProficientWeapons(self):
+        return self.proficientWeapons
+
+    def getHitDice(self):
+        return self.hitDice
+
+    def setSkills(self, newSkills):
+        self.skills = newSkills
+
+class Fighter (Class):
+    def __init__(self):
+        super().__init__()
+        self.classSkills = [Climb, Craft, Handle_Animal, Intimidate, Ride, Survival, Swim]
+        for skill in self.skills:
+            if skill in self.classSkills:
+                skill.setTrained()
+            if skill == Knowledge:
+                for subject in Knowledge:
+                    if subject.getName() == "Knowledge(Dungeoneering)" or subject.getName() == "Knowledge(Engineering)":
+                        subject.setTrained()
+            if skill == Profession:
+                for type in Profession:
+                    type.setTrained()
+
---
+++ Feats.py
@@ -0,0 +1,14 @@
+from Skills import *
+class Feat:
+    def __init__(self, name, effects):
+        self.name = name
+        self.effects = effects
+
+    def getName(self):
+        return self.name
+
+    def getEffect(self):
+        return self.getEffect()
+
+Low_Light_Vision = Feat("Low-Light Vision", "can see twice as far in dim light")
+Elven_Immunities = Feat("Elven Immunities", ["immune to magic sleep effects", "+2 saving throw bonus against enchantment spells and effects"])
